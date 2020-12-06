64005506366f7fa85e3ca3247bd80ffc27defbba
nekonokoto <53358798+nekonokoto@users.noreply.github.com>
2020-12-06 21:58:59 +0800

    Update Selling.java

--- Selling.java
+++ Selling.java
@@ -1,5 +1,4 @@
-/*彩蛋1：if not defining explicit constructor, Java provides a default/implicit constructor.
-彩蛋2：-d 產生資料夾。*/
+
 public class Selling {
   public static void main(String[] args){
     Ticket t1=new Ticket("2020/4/25",250);
